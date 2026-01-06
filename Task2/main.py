f08ca0d3f6df8b74df49d625af4b1c0f5e0eada2
Rida Batool <ridabatoolghori@gmail.com>
2026-01-06 22:13:52 +0500

    writting code

--- Task2/main.py
+++ Task2/main.py
@@ -2,5 +2,16 @@ def balancedchallenges(list_):
     sum_= sum(list_)
     if sum_%2!=0:
         return False
-    value= sum_/2
-    
+    value= sum_//2
+    dp=[False]*(value+1)
+    dp[0]=True
+    for s in list_:
+        for j in range(value, s-1, -1):
+            dp[j]= dp[j] or dp[j - s]
+           
+    return dp[value]
+print("first run  : [1, 5, 11, 5]")
+print(balancedchallenges([1, 5, 11, 5]))
+print("second run  : [1, 3, 5]")
+print(balancedchallenges([1, 3, 5]))
+
