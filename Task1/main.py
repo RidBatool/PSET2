d8ed9721cfb410d96724f2cd31c57ff6b5e83939
Rida Batool <ridabatoolghori@gmail.com>
2026-01-06 21:52:03 +0500

    removing error

--- Task1/main.py
+++ Task1/main.py
@@ -7,14 +7,16 @@ def mountainpeAK(n):
         elif value <0:
             return 
         path.append(1)
-        print("from first iteration", path)
         dfs(value-1, path)
         path.pop()
         path.append(2)
-        print("from second iteration", path)
         dfs(value-2, path)
         path.pop()
     dfs(n, [])
     return result
-
+print("first run")
+print(mountainpeAK(2))
+print("second run")
 print(mountainpeAK(3))
+print("third run")
+print(mountainpeAK(4))
