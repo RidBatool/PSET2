9585601b0781d8f2ec82047217818975f293403b
Rida Batool <ridabatoolghori@gmail.com>
2026-01-06 21:49:05 +0500

    Updating logic

--- Task1/main.py
+++ Task1/main.py
@@ -1,1 +1,20 @@
-print("Hello, World!")
+def mountainpeAK(n):
+    result=[]
+    def dfs(value, path):
+        if value == 0:
+            result.append(path.copy())
+            return
+        elif value <0:
+            return 
+        path.append(1)
+        print("from first iteration", path)
+        dfs(value-1, path)
+        path.pop()
+        path.append(2)
+        print("from second iteration", path)
+        dfs(value-2, path)
+        path.pop()
+    dfs(n, [])
+    return result
+
+print(mountainpeAK(3))
