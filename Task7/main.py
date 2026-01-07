bb1c9fc19144743c935c4fad73955171e486f252
Rida Batool <ridabatoolghori@gmail.com>
2026-01-07 23:26:20 +0500

    correcting logic

--- Task7/main.py
+++ Task7/main.py
@@ -1,12 +1,24 @@
 def calculate_minimum_speed(piles, k):
-    value = sum(piles)
-    print(value)
-    if value%k==0:
-        return value//k
-    else:
-        return (value//k)+1
+    left = 1
+    right = max(piles)
     
+    while left < right:
+        mid = (left + right) // 2
+        hours_needed = 0
+        for pile in piles:
+            hours_needed += pile // mid
+            if pile % mid != 0:
+                hours_needed += 1
+        
+        if hours_needed <= k:
+            right = mid
+        else:
+            left = mid + 1
+    
+    return f'{left} bananas/hour'
+
 print("First test case: ")
-print(calculate_minimum_speed([5, 10, 3], 4))
-print("Second test case: ")
-print(calculate_minimum_speed([5, 10, 15, 20], 7))
+print(calculate_minimum_speed([5, 10, 3], 4))   
+print("Second test case: ")       
+print(calculate_minimum_speed([5, 10, 15, 20], 7))     
+
