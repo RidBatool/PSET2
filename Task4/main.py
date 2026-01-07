e0d60bf7cc6b267f96d63abe39562dd5fdb956bd
Rida Batool <ridabatoolghori@gmail.com>
2026-01-07 22:13:47 +0500

    writting code

---
+++ Task4/main.py
@@ -0,0 +1,16 @@
+def count_payment_combinations(coins, total_sum, ind=0):
+    if total_sum == 0:
+        return 1
+    if total_sum < 0 or ind == len(coins):
+        return 0
+
+    include = count_payment_combinations(coins, total_sum - coins[ind], ind)
+    exclude = count_payment_combinations(coins, total_sum, ind + 1)
+
+    return include + exclude
+print("First test case: ")
+print(count_payment_combinations([1, 2, 3], 4))  # Output: 4
+print("second test case:")
+print(count_payment_combinations([2, 5, 3, 6], 10))  # Output: 5  
+print("third test case: ")
+print(count_payment_combinations([5],4))  # Output: 6  
