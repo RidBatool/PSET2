beb56a6c7975d45d797f0c1f378d65a86eb0342a
Rida Batool <ridabatoolghori@gmail.com>
2026-01-08 21:21:23 +0500

    updating

--- Task4/main.py
+++ Task4/main.py
@@ -1,13 +1,21 @@
-def count_payment_combinations(coins, total_sum, ind=0):
+def count_payment_combinations(coins, total_sum, ind=0, memo=None):
+    if memo is None:
+        memo = {}
+    
     if total_sum == 0:
         return 1
     if total_sum < 0 or ind == len(coins):
         return 0
+    
+    if (total_sum, ind) in memo:
+        return memo[(total_sum, ind)]
+    
+    include = count_payment_combinations(coins, total_sum - coins[ind], ind, memo)
+    exclude = count_payment_combinations(coins, total_sum, ind + 1, memo)
+    
+    memo[(total_sum, ind)] = include + exclude
+    return memo[(total_sum, ind)]
 
-    include = count_payment_combinations(coins, total_sum - coins[ind], ind)
-    exclude = count_payment_combinations(coins, total_sum, ind + 1)
-
-    return include + exclude
 print("First test case: ")
 print(count_payment_combinations([1, 2, 3], 4)) 
 print("second test case:")
